91f8d8d7c85a6cf0d6cfab2c1c5135ca931960da
jesus.pbarrera <jesus.pbarrera@alumnos.udg.mx>
2022-01-27 12:32:28 -0600

    initial canvas

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Press the green button in the gutter to run the script.
+from tkinter import *
+if __name__ == '__main__':
+    my_window = Tk()
+    my_canvas = Canvas(my_window, width=500, height=500, background='white')
+    my_canvas.grid(row=0, column=0)
+    my_window.mainloop()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
