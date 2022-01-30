54d35f8ad3a6bac9bdb782b8a91a9d51c9d7ab62
María Cano <maria.cano4435@alumnos.udg.mx>
2022-01-30 12:05:39 -0600

    El cascaron del algorito perceptron es independiente de la ventana

---
+++ perceptron.py
@@ -0,0 +1,21 @@
+from random import *
+import windows
+
+def inicialize_w(long_vw):
+    for i in range(long_vw+1):
+        w[i]= random.random()
+
+    return w
+
+
+def perceptron():
+    v_w=inicialize_w() # needs catch long of w
+    pass
+
+
+def run():
+   windows.run()
+
+
+if __name__ == '__main__':
+    run()
