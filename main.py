f3955e0f8980499b4a375e4b10d679629388d7d2
jesus.pbarrera <jesus.pbarrera@alumnos.udg.mx>
2022-01-29 21:25:06 -0600

    Plot redone using Figure and FigureCanvas instead of Canvas.

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-# Press the green button in the gutter to run the script.
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
+from tkinter.ttk import *
+import matplotlib
+matplotlib.use("TkAgg")
 
 
 def display_coordinates(event):
     my_label["text"] = f'x={event.x} y={event.y}'
 
 
-if __name__ == '__main__':
-    my_window = Tk()
-    my_window.geometry("800x400")
-    my_canvas = Canvas(my_window, width=400, height=400, background='white')
-    my_canvas.pack()
-    my_label = Label(bd=4, relief="solid", font="Times 22 bold", bg="white", fg="black")
-    my_canvas.bind('<Button-1>', display_coordinates)
-    my_canvas.grid(row=0, column=0)
-    my_label.grid(row=0, column=1)
-    my_window.mainloop()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# TK Window
+root = Tk()
+# Creating figure
+figure = Figure(figsize=(5, 4), dpi=100)
+plot = figure.add_subplot(1, 1, 1)
+plot.axis([-1, 1, -1, 1])
+# adding vertical line in data co-ordinates
+plot.axvline(0, c='black', ls='--')
+# adding horizontal line in data co-ordinates
+plot.axhline(0, c='black', ls='--')
+# Creating canvas & label
+canvas = FigureCanvasTkAgg(figure, root)
+my_label = Label(relief="solid", font="Times 22 bold")
+# Displaying canvas and label
+canvas.get_tk_widget().grid(row=0, column=0)
+canvas.get_tk_widget().bind('<Button-1>', display_coordinates)
+my_label.grid(row=0, column=1)
+# Run mainloop
+root.mainloop()
